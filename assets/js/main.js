12990277927fbd693449aeb61b8ab24a19be8409
VitalieVS <54939321+VitalieVS@users.noreply.github.com>
2019-12-02 20:02:36 +0200

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,7 @@
 $(document).ready(function () {
+    let cartPrice = [];
+    let cartCount = 0;
+    let totalPriceItems = 0;
 
     $(".content-item").click(function () {
         alert("Handler for .click() called.");
@@ -9,7 +12,36 @@ $(document).ready(function () {
     });
 
     $('.item-price').click(function () {
-        alert($(this).attr("data-value") * 1);
+        let itemPrice = ($(this).attr("data-value") * 1);
+        priceAdd(itemPrice);
+        incrementCartItems();
+        cartDisplay();
     });
 
+    function incrementCartItems() {
+        cartCount++;
+    }
+
+    function cartDisplay() {
+        document.getElementById("cart-count").innerHTML = cartCount;
+        document.getElementById("total-price").innerHTML = totalPriceItems;
+    }
+
+    function priceAdd(itemPrice) {
+        cartPrice.push(itemPrice);
+        cartSum();
+        priceClear();
+    }
+
+    function priceClear() {
+        cartPrice = [];
+    }
+
+    function cartSum() {
+        cartPrice.forEach(totalPrice);
+    }
+
+    function totalPrice(item) {
+        totalPriceItems += item;
+    }
 });
